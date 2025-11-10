c234fe28426438bbfd64cf2a91599c6f7def22c6
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2025-11-09 20:09:33 -0400

    feat: crate and add the mainTemplate component and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,10 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
 
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { Footer } from './components/Footer';
-import { MainForm } from './components/MainForm';
+import { Home } from './pages/Home';
 
 function App() {
-  return (
-      <>
-        <Container>
-          <Logo/>
-        </Container>
-
-        <Container>
-          <Menu />
-        </Container>
-
-        <Container>
-          <CountDown />
-        </Container>
-
-        <Container>
-          <MainForm />
-        </Container>
-
-        <Container>
-          <Footer />
-        </Container>
-      </>
-  );
+  return <Home />
 }
 
 export default App;
