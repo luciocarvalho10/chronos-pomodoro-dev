17a0d59c52d32cc192fecab88c966a13836d7358
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2025-11-16 13:49:47 -0400

    feat: add the new task structure

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,23 +1,77 @@
-import styles from './styles.module.css';
-import { DefaultInput } from '../DefaultInput';
-import { Cycles } from '../Cycles';
-import { DefaultButton } from '../Defaultbutton';
-import { PlayCircleIcon } from 'lucide-react';
+import { useTask }                from '../../contexts/TaskContext/useTask.ts';
+import { type TaskModel }         from '../../models/TaskModel.tsx';
+import { type TaskStateModel }    from '../../models/TaskStateModel.tsx';
+import styles                     from './styles.module.css';
+import { DefaultInput }           from '../DefaultInput';
+import { Cycles }                 from '../Cycles';
+import { DefaultButton }          from '../Defaultbutton';
+import { PlayCircleIcon }         from 'lucide-react';
+import { type FormEvent, useRef } from 'react';
 
 export function MainForm() {
+  const taskNameInput = useRef<HTMLInputElement>( null );
+  const {setState} = useTask();
+  
+  function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    
+    if ( !taskNameInput.current ) return;
+    
+    const taskName = taskNameInput.current.value.trim();
+    
+    if ( !taskName ) {
+      alert( 'Por favor, digite um nome para a tarefa.' );
+      return;
+    }
+    
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      duration: 1,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      type: 'workTime'
+    };
+    
+    const secondsRemaining = newTask.duration * 60;
+    
+    setState( (prevState: TaskStateModel) => {
+      return {
+        ...prevState,
+        tasks: [ ...prevState.tasks, newTask ],
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        activeTask: newTask,
+        currentCycle: 1, //conferir
+        config: {...prevState.config}
+      };
+    } );
+  }
+  
   return (
-      <form className={styles.form} action="">
-        <div className={styles.formRow}>
-          <DefaultInput placeholder={'Digite algo...'} labelText={'task'} id={'meuInput'} type='text' />
-        </div>
-
-        <div className={styles.formRow}>
-          <Cycles />
-        </div>
-
-        <div className={styles.formRow}>
-          <DefaultButton type={'submit'} icon={<PlayCircleIcon />} />
-        </div>
-      </form>
+    <form onSubmit={handleCreateNewTask} className={styles.form} action="">
+      <div className={styles.formRow}>
+        <DefaultInput
+          type="text"
+          placeholder={'Digite algo...'}
+          labelText={'task'}
+          id={'meuInput'}
+          ref={taskNameInput}
+        />
+      </div>
+      
+      <div className={styles.formRow}>
+        <Cycles/>
+      </div>
+      
+      <div className={styles.formRow}>
+        <DefaultButton
+          type={'submit'}
+          title={'Botão Iniciar Tarefa'}
+          icon={<PlayCircleIcon/>}
+        />
+      </div>
+    </form>
   );
 }
--- src/contexts/TaskContext/TaskProvider.tsx
+++ src/contexts/TaskContext/TaskProvider.tsx
@@ -1,21 +1,21 @@
 import { type ReactNode, useMemo, useState } from 'react';
-import type { TaskStateModel } from '../../models/TaskStateModel.tsx';
-import { initialTaskState } from './initialTaskState.ts';
-import { TaskContext } from "./TaskContext.tsx";
+import type { TaskStateModel }               from '../../models/TaskStateModel.tsx';
+import { initialTaskState }                  from './initialTaskState.ts';
+import { TaskContext }                       from "./TaskContext.tsx";
 
 type TaskProviderProps = { children?: ReactNode };
 
-export function TaskProvider ({ children }: TaskProviderProps) {
-  const [state, setState] = useState<TaskStateModel>(initialTaskState);
-
-  const value = useMemo(() => ({
+export function TaskProvider({children}: TaskProviderProps) {
+  const [ state, setState ] = useState<TaskStateModel>( initialTaskState );
+  
+  const value = useMemo( () => ( {
     state,
     setState,
-  }),[state, setState])
-
+  } ), [ state, setState ] );
+  
   return (
-      <TaskContext.Provider value={value}>
-        {children}
-      </TaskContext.Provider>
+    <TaskContext.Provider value={value}>
+      {children}
+    </TaskContext.Provider>
   );
 }
