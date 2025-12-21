a82e5171f58968620240112b2774521518b4af34
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2025-12-21 19:00:56 -0400

    feat: extract and centralize routing logic
    - Create `MainRouter` component to handle routing
    - Replace direct `BrowserRouter` usage in `App` with `MainRouter`
    - Create scroll to top function

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,14 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { BrowserRouter, Route, Routes } from 'react-router';
-import { MessagesContainer }            from './components/MessagesContainer';
-import { Providers }                    from './contexts/providers.tsx';
-import { AboutPomodoro }                from './pages/AboutPomodoro';
-import { Home }                         from './pages/Home';
-import { NotFound }                     from './pages/NotFound';
+import { MessagesContainer } from './components/MessagesContainer';
+import { Providers }         from './contexts/providers.tsx';
+import { MainRouter }        from './router/MainRouter';
 
 function App() {
   return (
     <Providers >
       <MessagesContainer >
-        <BrowserRouter >
-          <Routes >
-            <Route
-              path="/"
-              element={<Home />}
-            />
-            
-            <Route
-              path="/about-pomodoro"
-              element={<AboutPomodoro />}
-            />
-            
-            <Route
-              path="*"
-              element={<NotFound />}
-            />
-          </Routes >
-        </BrowserRouter >
+        <MainRouter />
       </MessagesContainer >
     </Providers >
   );
---
+++ src/router/MainRouter/index.tsx
@@ -0,0 +1,40 @@
+import { useEffect }                                 from 'react';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
+import { AboutPomodoro }                             from '../../pages/AboutPomodoro';
+import { Home }                                      from '../../pages/Home';
+import { NotFound }                                  from '../../pages/NotFound';
+
+function ScrollToTop() {
+  const {pathname} = useLocation();
+  
+  useEffect( () => {
+    // window.scrollTo( 0, 0 );
+    window.scrollTo( {top: 0, behavior: 'smooth'} );
+  }, [ pathname ] );
+  
+  return null;
+}
+
+export function MainRouter() {
+  return (
+    <BrowserRouter >
+      <Routes >
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        
+        <Route
+          path="/about-pomodoro"
+          element={<AboutPomodoro />}
+        />
+        
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes >
+      <ScrollToTop />
+    </BrowserRouter >
+  );
+}
