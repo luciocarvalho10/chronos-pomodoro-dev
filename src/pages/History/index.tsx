0a3c69282f95b54bba5a8df2153cdace418e87f6
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2026-01-04 21:51:21 -0400

    feat: add task sorting on History page
    - Implement `sortTasks` utility for dynamic task sorting
    - Introduce click-to-sort functionality for task table headers
    - Update styles to enable sorting indicators
    - Enhance date formatting with seconds precision

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,16 +1,39 @@
-import { TrashIcon }     from 'lucide-react';
-import { Container }     from '../../components/Container';
-import { DefaultButton } from '../../components/Defaultbutton';
-import { Heading }       from '../../components/Heading';
-import { useTask }       from '../../contexts/TaskContext/useTask.ts';
-import { MainTemplate }  from '../../templates/MainTemplate';
-import { formatDate }    from '../../utils/formatDate.ts';
-import { getTaskStatus } from '../../utils/getTaskStatus.ts';
-import { getTaskType }   from '../../utils/getTaskType.ts';
-import styles            from './style.module.css';
+import { TrashIcon }                        from 'lucide-react';
+import { useState }                         from 'react';
+import { Container }                        from '../../components/Container';
+import { DefaultButton }                    from '../../components/Defaultbutton';
+import { Heading }                          from '../../components/Heading';
+import { useTask }                          from '../../contexts/TaskContext/useTask.ts';
+import { MainTemplate }                     from '../../templates/MainTemplate';
+import { formatDate }                       from '../../utils/formatDate.ts';
+import { getTaskStatus }                    from '../../utils/getTaskStatus.ts';
+import { getTaskType }                      from '../../utils/getTaskType.ts';
+import { sortTasks, type SortTasksOptions } from '../../utils/sortTasks.ts';
+import styles                               from './style.module.css';
 
 export function History() {
   const {state} = useTask();
+  const [ sortTasksOptions, setSortTasksOptions ] = useState<SortTasksOptions>(
+    () => ( {
+      tasks: sortTasks( {tasks: state.tasks} ),
+      field: 'startDate',
+      direction: 'desc'
+    } )
+  );
+  
+  function handleSortTasks({field}: Pick<SortTasksOptions, 'field'>) {
+    const newDirection = sortTasksOptions.direction === 'asc' ? 'desc' : 'asc';
+    setSortTasksOptions( {
+      tasks: sortTasks( {
+        direction: newDirection,
+        tasks: sortTasksOptions.tasks,
+        field
+      } ),
+      direction: newDirection,
+      field
+    } );
+  }
+  
   return (
     <MainTemplate >
       <Container >
@@ -32,16 +55,28 @@ export function History() {
           <table >
             <thead >
             <tr >
-              <th >Tarefa</th >
-              <th >Duração</th >
-              <th >Data</th >
+              <th
+                onClick={() => handleSortTasks( {field: 'name'} )}
+                className={styles.thSort}
+              >Tarefa ↕️
+              </th >
+              <th
+                onClick={() => handleSortTasks( {field: 'duration'} )}
+                className={styles.thSort}
+              >Duração ↕️
+              </th >
+              <th
+                onClick={() => handleSortTasks( {field: 'startDate'} )}
+                className={styles.thSort}
+              >Data ↕️
+              </th >
               <th >Status</th >
               <th >Tipo</th >
             </tr >
             </thead >
             
             <tbody >
-            {state.tasks.map( (task) => (
+            {sortTasksOptions.tasks.map( (task) => (
               <tr key={task.id}>
                 <td >{task.name}</td >
                 <td >{task.duration}min</td >
