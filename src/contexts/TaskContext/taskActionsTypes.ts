4bf59221fceaa37ef2e581476061d58fb4b7467e
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2026-02-04 23:10:24 -0400

    feat: update task action types definition and remove unused vite.svg file
    - Replace enum with a const object for `TaskActionTypes`
    - Adjust type definitions to use `typeof` for action types
    - Delete unused `vite.svg` asset file

--- src/contexts/TaskContext/taskActionsTypes.ts
+++ src/contexts/TaskContext/taskActionsTypes.ts
@@ -1,34 +1,34 @@
 import type { TaskModel }      from '../../models/TaskModel.tsx';
 import type { TaskStateModel } from '../../models/TaskStateModel.tsx';
 
-export enum TaskActionTypes {
-  CHANGE_SETTINGS = 'CHANGE_SETTINGS',
-  COMPLETE_TASK = 'COMPLETE_TASK',
-  COUNT_DOWN = 'COUNT_DOWN',
-  INTERRUPT_TASK = 'INTERRUPT_TASK',
-  RESET_STATE = 'RESET_STATE',
-  START_TASK = 'START_TASK',
-}
+export const TaskActionTypes = {
+  CHANGE_SETTINGS: 'CHANGE_SETTINGS',
+  COMPLETE_TASK: 'COMPLETE_TASK',
+  COUNT_DOWN: 'COUNT_DOWN',
+  INTERRUPT_TASK: 'INTERRUPT_TASK',
+  RESET_STATE: 'RESET_STATE',
+  START_TASK: 'START_TASK',
+} as const;
 
 type ChangeSettings = {
-  type: TaskActionTypes.CHANGE_SETTINGS;
+  type: typeof TaskActionTypes.CHANGE_SETTINGS;
   payload: TaskStateModel['config']
 }
 type CompleteTask = {
-  type: TaskActionTypes.COMPLETE_TASK;
+  type: typeof TaskActionTypes.COMPLETE_TASK;
 }
 type CountDown = {
-  type: TaskActionTypes.COUNT_DOWN;
+  type: typeof TaskActionTypes.COUNT_DOWN;
   payload: { secondsRemaining: number }
 }
 type InterruptTask = {
-  type: TaskActionTypes.INTERRUPT_TASK;
+  type: typeof TaskActionTypes.INTERRUPT_TASK;
 }
 type ResetState = {
-  type: TaskActionTypes.RESET_STATE;
+  type: typeof TaskActionTypes.RESET_STATE;
 }
 type StartTask = {
-  type: TaskActionTypes.START_TASK;
+  type: typeof TaskActionTypes.START_TASK;
   payload: TaskModel
 }
 
